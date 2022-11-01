61aafb9ee376f62cb6ac26d8ee8ec9e2d08af89d
SebastianAndresGarcia <sebastiana.garcia@alumnos.frm.utn.edu.ar>
2022-11-01 19:42:58 -0300

    1

--- backend/src/controllers/ArticuloManufacturadoController.js
+++ backend/src/controllers/ArticuloManufacturadoController.js
@@ -1,34 +1,32 @@
 const ArticuloManufacturado = require('../models/ArticuloManufacturado');
 const DetalleArticuloManufacturado = require('../models/DetalleArticuloManufacturado');
-const ArticuloInsumo=require('../models/ArticuloInsumo');
+const ArticuloInsumo = require('../models/ArticuloInsumo');
 const RubroGeneral = require('../models/RubroGeneral');
 
 
 exports.createArticuloManufacturado = async (req, res) => {
-const insumo = new ArticuloInsumo({ 
-    denominacion: req.body.denominacion,
-    precioCompra: req.body.precioCompra,
-	precioVenta: req.body.precioVenta,
-    stockActual: req.body.stockActual,
-	stockMinimo: req.body.stockMinimo,
-	unidadMedida: req.body.unidadMedida,
-	esInsumo: req.body.esInsumo  
-})
-const detalle = new DetalleArticuloManufacturado({
-    cantidad: req.body.cantidad, 
-    unidadMedida: req.body.unidadMedida
-})
-    //const user = await User.findById(req.body.id)
-    const articulo = new ArticuloManufacturado(req.body)
-    console.log(articulo)
-    try {
-        const savedArticulo = await articulo.save()
-        const rubroGralUpdated =await RubroGeneral.findByIdAndUpdate(req.body.rubrogeneralid, {$addToSet:{"articulomanufacturadoid":savedArticulo._id}})
-        console.log(savedArticulo)
-        res.json(rubroGralUpdated)
-        
-    } catch (error) {
-        console.log(error)
-    }
 
+    const insumo = req.body.insumos;
+    console.log(insumo);
+    const producto = req.body.ArticuloManufacturado
+    console.log(producto)
+    /*const insumo = await ArticuloInsumo.findOne({ denominacion: req.body.denominacion })
+    const detalle = new DetalleArticuloManufacturado({
+        cantidad: req.body.cantidad,
+        unidadMedida: req.body.unidadMedida
+    })
+    //const user = await User.findById(req.body.id)
+    if (insumo) {
+        const articulo = new ArticuloManufacturado(req.body)
+        console.log(articulo)
+        try {
+            const savedArticulo = await articulo.save()
+            const rubroGralUpdated = await RubroGeneral.findByIdAndUpdate(req.body.rubrogeneralid, { $addToSet: { "articulomanufacturadoid": savedArticulo._id } })
+            console.log(savedArticulo)
+            res.json(rubroGralUpdated)
+
+        } catch (error) {
+            console.log(error)
+        }
+    } */
 }
