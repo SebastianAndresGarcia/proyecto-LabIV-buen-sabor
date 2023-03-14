dfc5f2b7b799a3d824511620f4812b33aa7b5345
SebastianAndresGarcia <sebastiana.garcia@alumnos.frm.utn.edu.ar>
2023-03-14 19:50:01 -0300

    idem

--- frontend/src/funciones/CalculaCosto.js
+++ frontend/src/funciones/CalculaCosto.js
@@ -1,4 +1,4 @@
-async function calculaCosto(detalle) { 
+async function calculaCosto(detalle) {
     //DETALLE:
     // articulomanufacturadoid 
     //      denominacion
@@ -12,15 +12,25 @@ async function calculaCosto(detalle) {
     if (detalle[0]) {
         let costo = 0
         for (let i = 0; i < detalle.length; i++) {
-            const res = await fetch(
-                `http://localhost:3000/getManufacturadoXid/${detalle[i].articulomanufacturadoid._id}`
-            )
-
-            const resJson = await res.json();
-            const array = resJson.detallearticulomanufacturadoid
-            console.log("detalleartmanufacturado ", array)
-            for (let j = 0; j < array.length; j++) {
-                costo = costo + [array[j].cantidad * (array[j].ArticuloInsumoid.precioCompra)]*detalle[i].cantidad
+            try {
+                const res = await fetch(
+                    `http://localhost:3000/getManufacturadoXid/${detalle[i].articulomanufacturadoid._id}`
+                )
+                const resJson = await res.json();
+                const array = resJson.detallearticulomanufacturadoid
+                console.log("detalleartmanufacturado ", array)
+                for (let j = 0; j < array.length; j++) {
+                    costo = costo + [array[j].cantidad * (array[j].ArticuloInsumoid.precioCompra)] * detalle[i].cantidad
+                }
+            }
+            catch {
+                const res = await fetch(
+                    `http://localhost:3000/ArticuloInsumoxid/${detalle[i].ArticuloInsumoid._id}`
+                )
+                const resJson = await res.json();
+                const precioCompra = resJson.precioCompra
+                console.log("precioCompra insumo ", precioCompra)
+                costo = costo + precioCompra * detalle[i].cantidad
             }
         }
         console.log("costo " + costo)
